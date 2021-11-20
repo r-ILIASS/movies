3895dc6e86c3a8933329fa80a15b675524d6ceb1
r-ILIASS <iliass.root@gmail.com>
2021-11-20 14:39:20 +0100

    Feat: Add sort icon

--- src/components/common/TableHeader.jsx
+++ src/components/common/TableHeader.jsx
@@ -12,15 +12,24 @@ const TableHeader = ({ columns, sortColumn, onSort }) => {
     onSort(tmpSortColumn);
   };
 
+  const renderSortIcon = (column) => {
+    if (column.path !== sortColumn.path) return null;
+    if (sortColumn.order === "asc") return <i className="fa fa-sort-asc"></i>;
+    return <i className="fa fa-sort-desc"></i>;
+  };
+
   return (
     <thead>
       <tr>
         {columns.map((column) => (
           <th
+            className="clickable"
             key={column.path || column.key}
             onClick={() => raiseSort(column.path)}
           >
             {column.label}
+            <span style={{ paddingRight: "0.7rem" }}></span>
+            {renderSortIcon(column)}
           </th>
         ))}
       </tr>

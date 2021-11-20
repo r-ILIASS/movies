a7888ecdbf484fb45409d22f1d761c1d90c71c7e
r-ILIASS <iliass.root@gmail.com>
2021-11-20 14:57:40 +0100

    Refactor: Extract the wholee table to a reusable table component

--- src/components/MoviesTable.jsx
+++ src/components/MoviesTable.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import TableHeader from "../components/common/TableHeader";
-import TableBody from "./common/TableBody";
+import Table from "./common/Table";
 import Like from "../components/common/Like";
 
 const MoviesTable = ({
@@ -42,35 +41,12 @@ const MoviesTable = ({
           ? "There is one movie in the database"
           : `There are ${moviesCount} movies in the database.`}
       </h3>
-      <table className="table">
-        <TableHeader
-          columns={columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody data={currentMovies} columns={columns} />
-        {/* <tbody>
-          {currentMovies.map((movie) => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like handleLike={() => onLike(movie)} liked={movie.liked} />
-              </td>
-              <td>
-                <button
-                  onClick={() => onDelete(movie)}
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody> */}
-      </table>
+      <Table
+        columns={columns}
+        sortColumn={sortColumn}
+        onSort={onSort}
+        data={currentMovies}
+      />
     </>
   );
 };

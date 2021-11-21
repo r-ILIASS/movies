eb377c045243d58760ef635d6e27990b003e5556
r-ILIASS <iliass.root@gmail.com>
2021-11-21 15:29:32 +0100

    Feat: Handle input change

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginForm = () => {
+  const [account, setAccount] = useState({ username: "", password: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
+  const handleChange = ({ currentTarget: input }) => {
+    const tmpAccount = { ...account };
+    tmpAccount[input.name] = input.value;
+    setAccount(tmpAccount);
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="username" className="form-label">
-            Email address
+            Username
           </label>
-          <input type="text" className="form-control" id="username" />
+          <input
+            id="username"
+            name="username"
+            value={account.username}
+            onChange={handleChange}
+            className="form-control"
+            type="text"
+            autoFocus
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input type="password" className="form-control" id="password" />
+          <input
+            id="password"
+            name="password"
+            onChange={handleChange}
+            value={account.password}
+            className="form-control"
+            type="password"
+          />
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
